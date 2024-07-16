7b783d48f5c9ccc4dc51018a05c4a183e87e49bf
Artyom <agrhxd@yandex.ru>
2024-07-16 14:51:58 +0300

    updates for test tcp client + server code

--- source/echo_tests/tcp_server.py
+++ source/echo_tests/tcp_server.py
@@ -7,3 +7,14 @@ PORT = 54321
 
 tcp_server_socket.bind((HOST, PORT))
 tcp_server_socket.listen()
+
+while True:
+    client_socket, client_adress = tcp_server_socket.accept()
+    print(type(client_socket))
+    print(client_socket)
+    print(type(client_adress))
+    print(client_adress)
+
+    print(f'Connected to {client_socket}')
+    client_socket.send("You are connected!".encode())
+    
