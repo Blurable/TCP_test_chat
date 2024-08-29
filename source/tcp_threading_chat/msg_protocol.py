ab238845cf588fd0132634cf86aaf139185e8b14
Artyom <agrhxd@yandex.ru>
2024-08-29 12:27:59 +0300

    tryint to create protocol

--- source/tcp_threading_chat/msg_protocol.py
+++ source/tcp_threading_chat/msg_protocol.py
@@ -1,45 +1,41 @@
-from enum import Enum
-from typing import NamedTuple
-import re
-
-
-class Call(NamedTuple):
-    text: str
-    id: int
-
-class Calls(Enum):
-    MSG = Call(r'.*', 0)
-    INFO = Call('\\info', 1)
-    MEMBERS = Call('\\members', 2)
-    ALL_CHAT = Call('\\all', 3)
-    DIRECT_MESSAGES = Call(r'\\[a-zA-Z]+', 4)
-
-
-
 class MsgProtocol:
 
+    def __init__(self, chat_commands: list, encoder: str = 'utf-8', bytesize: int = 1024):
+        self.chat_command_map: dict = {command: i for i, command in enumerate(chat_commands)}
+        self.encoder = encoder
+        self.bytesize = bytesize
 
 
-    @staticmethod
-    def encode(msg: str):
-        encoded_msg = ''
-        match msg:
-            case Calls.INFO.value.text:
-                encoded_msg += Calls.INFO.value.id
-            case Calls.MEMBERS.value.text:
-                encoded_msg += Calls.MEMBERS.value.id
-            case Calls.ALL_CHAT.value.text:
-                encoded_msg += Calls.ALL_CHAT.value.id
-            case msg if re.match(Calls.DIRECT_MESSAGES.value.text, msg):
-                encoded_msg += Calls.DIRECT_MESSAGES.value.id
-            case Calls.INFO.value.text:
-                encoded_msg += Calls.INFO.value.id
-            case Calls.INFO.value.text:
-                encoded_msg += Calls.INFO.value.id
+    def encode(self, msg: str) -> bytes:
+
+        if msg in self.chat_command_map:
+            encoded_msg = self.chat_command_map[msg].to_bytes(1, byteorder='big')
+            return encoded_msg
+        encoded_msg = (0).to_bytes(1, byteorder='big')
         
-        return msg
-    
-    @staticmethod
-    def decode(msg: str):
+        try:
+            msg_length = len(msg)
+            msg_byte_length = msg_length.to_bytes(4, byteorder='big')
+            encoded_msg += msg_byte_length
+        except OverflowError:
+            print('message is too big')
 
-        return msg
+        encoded_msg += msg.encode(self.encoder)      
+        
+        return encoded_msg
+    
+
+    def decode_command(self, command_id: bytes) -> str:
+        command_id = int.from_bytes(command_id, byteorder='big')
+        for key, val in self.chat_command_map:
+            if val == command_id:
+                return key
+    
+
+    def decode_length(self, length: bytes) -> int:
+        length = int.from_bytes(length, bytetsize='big')
+        return length
+
+
+    def decode_msg(self, msg: bytes) -> str:
+        return msg.decode(self.encoder)
